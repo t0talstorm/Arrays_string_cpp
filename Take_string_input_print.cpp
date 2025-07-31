8bede04f62c45dc552f1b3ff12eba7a39fe4808d
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2025-07-30 23:53:47 -0400

    Experiment 7E

---
+++ Take_string_input_print.cpp
@@ -0,0 +1,32 @@
+//Name
+// Name: Aditya Sinha
+// PRN: 24070123006
+//Batch: EnTc A1
+//Experiment: 7E
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+
+    string str; 
+
+    cout << "Enter any string: ";
+
+   cin >> str;
+
+    cout << "You entered: " << str << endl;
+
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter any string: HelloWorld
+You entered: HelloWorld
+
+
+=== Code Execution Successful ===
+
+*/
